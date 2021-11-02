eca45b579912415f3307d4956d4a129a5b6daec3
adu <nogo22@naver.com>
2021-11-03 05:16:40 +0900

    컴퓨터와 끝말잇기 구현(버그 1개 발견), 두음법칙 적용

--- src/application/Play2048Controller.java
+++ src/application/Play2048Controller.java
@@ -1,11 +1,13 @@
 package application;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Stack;
 
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,73 +17,229 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class Play2048Controller {
 
-    @FXML	private GridPane board2048;
-    @FXML   private Label lblScore;
-    @FXML   private Label lblBestScore;
-    private byte boardSize = 4;
-    private Stage stage;
-    Stack stackTile = new Stack();
-    Label temp;
-    @FXML
-    void onPressKey(KeyEvent e) {
-       	if( e.getCode() == KeyCode.UP) {
-    		System.out.println("UP");
-    	}
-    	if( e.getCode() == KeyCode.DOWN) {
-    		System.out.println("DOWN");
-    	}
-    	if( e.getCode() == KeyCode.LEFT) {
-    		System.out.println("LEFT");
-    	}
-    	if( e.getCode() == KeyCode.RIGHT) {
-    		System.out.println("RIGHT");
-    	}
-    }
-    // 색은 인터넷에서 퍼와서 바꿔야될수도
-    void setColor(Label tile) {
-    	if(tile.getText()=="2")		tile.setStyle("-fx-background-color:#D5CEC4; -fx-text-fill:#6D675D;"); //2
-    	if(tile.getText()=="4")		tile.setStyle("-fx-background-color:#D2CAB5; -fx-text-fill:#6A645B;"); //4
-    	if(tile.getText()=="8")		tile.setStyle("-fx-background-color:#D1A272; -fx-text-fill:#E4DFBB;"); //8
-    	if(tile.getText()=="16")	tile.setStyle("-fx-background-color:#D08B5F; -fx-text-fill:#E1D3C5;"); //16
-    	if(tile.getText()=="32")	tile.setStyle("-fx-background-color:#CF775A; -fx-text-fill:#E1D1C3;"); //32
-    	if(tile.getText()=="64")	tile.setStyle("-fx-background-color:#CF5F40; -fx-text-fill:#E2D5C6;"); //64
-    	if(tile.getText()=="128")	tile.setStyle("-fx-background-color:#D1BA6F; -fx-text-fill:#E4DEC3;"); //128
-    	if(tile.getText()=="256")	tile.setStyle("-fx-background-color:#D4BD54; -fx-text-fill:#D8D6A9;"); //256
-    	if(tile.getText()=="512")	tile.setStyle("-fx-background-color:#CCB664; -fx-text-fill:#E2D7BC;"); //512
-    	if(tile.getText()=="1024")	tile.setStyle("-fx-background-color:#CEB349; -fx-text-fill:#E4E1BB;"); //1024
-    	if(tile.getText()=="2048")	tile.setStyle("-fx-background-color:#D7C647; -fx-text-fill:#E2D8BA;"); //2048
-    	if(Integer.parseInt(tile.getText())>=4096)	tile.setStyle("-fx-background-color:#37332E; -fx-text-fill:#C0BEBA;"); //4096
-    }
-    void initBoard() {
-//    	board2048.getChildren().clear();
-//    	for(int i =0 ; i<boardSize ; i++) {
-//    		for(int k=0 ; k<boardSize ; k++) {
-//    			stackTile.push(new Label(i+","+k));
-//    			temp = (Label) stackTile.peek();
-//    			temp.setPrefSize(99, 99);
-//    			board2048.add(temp, i, k);
-//    		}
-//    	}
-//    	
-    }
+	@FXML	private GridPane board2048;
+	@FXML   private Label lblScore;
+	@FXML   private Label lblBestScore;
+	private byte boardSize = 4;
+	ArrayList<Integer> boardZeroCheckX = new ArrayList<Integer>();
+	ArrayList<Integer> boardZeroCheckY = new ArrayList<Integer>();
+	private Stage stage;
+	private int[][] board = new int[boardSize][boardSize];
+	Stack stackTile = new Stack();
+	Label temp;
+	private boolean doubleSumProtection = false;
+	private int randomCoord, randomNewTileText;
+
+	//배열로 관리하는 보드 초기화
+	void initArrayBoard() {
+		for(int i = 0 ; i<boardSize ; i++ ) {
+			for(int k = 0 ; k<boardSize ; k++) {
+				board[k][i] = 0;
+			}
+		}
+	}
+
+	// 새로운 타일을 화면에 추가할때 타일이 없는 곳을 찾아서 ArrayList에 넣어준다.
+	void boardZeroCheck(){
+		boardZeroCheckX.clear();
+		boardZeroCheckY.clear();
+		for(int i = 0 ; i<boardSize ; i++ ) {
+			for(int k = 0 ; k<boardSize ; k++) {
+				if(board[k][i]==0) {
+					boardZeroCheckX.add(k);
+					boardZeroCheckY.add(i);
+				}
+			}
+		}
+	}
+	//    내가 생각한 2048 로직															| (0,0) | (1,0) | (2,0) | (3,0) |
+//    1. 해당 방향으로 최대한 타일을 밀기											| (0,1) | (1,1) | (2,1) | (3,1) |
+//    2. 해당 방향으로 근접한 위치에 같은 수가 있는경우 합치기						| (0,2) | (1,2) | (2,2) | (3,2) |
+//    3. 새로운 타일을 생성															| (0,3) | (1,3) | (2,3) | (3,3) |
+	@FXML
+	// 키보드 입력이 있는 경우
+	void onPressKey(KeyEvent e) {
+		if( e.getCode() == KeyCode.UP) {
+			for(int g = 0 ; g<board.length ; g++) {
+				// 1.  해당 방향으로 최대한 타일을 밀기
+				for(int i = 0 ; i<board.length ; i++) {
+					for(int k = 0 ; k<board.length-1 ; k++) {
+						if(board[i][k] == 0) {
+							board[i][k] = board[i][k+1];
+							board[i][k+1] = 0;
+						}
+					}
+				}
+			}
+			// 2. 해당 방향 부근에 인접한 숫자가 같은 숫자일경우 합친다.
+			for(int i = 0 ; i<board.length ; i++) {
+				for(int k = board.length-1 ; k>0 ; k--) {
+					if((board[i][k]==board[i][k-1])&&(!doubleSumProtection)&&(board[i][k]!=0)) {
+						board[i][k-1] *= 2;
+						board[i][k] = 0;
+						doubleSumProtection = true;
+					}
+				}
+				doubleSumProtection = false;
+			}
+			// 3번 새로운 타일을 생성
+			newTileAdd();
+		}
+		if( e.getCode() == KeyCode.DOWN) {
+			for(int g = 0 ; g<board.length ; g++) {
+				// 1.  해당 방향으로 최대한 타일을 밀기
+				for(int i = 0 ; i<board.length ; i++) {
+					for(int k = board.length-1 ; k>0 ; k--) {
+						if(board[i][k] == 0) {
+							board[i][k] = board[i][k-1];
+							board[i][k-1] = 0;
+						}
+					}
+				}
+			}
+			// 2. 해당 방향 부근에 인접한 숫자가 같은 숫자일경우 합친다.
+			for(int i = 0 ; i<board.length ; i++) {
+				for(int k = 0 ; k<board.length-1 ; k++) {
+					if((board[i][k]==board[i][k+1])&&(!doubleSumProtection)&&(board[i][k]!=0)) {
+						board[i][k+1] *= 2;
+						board[i][k] = 0;
+						doubleSumProtection = true;
+					}
+				}
+				doubleSumProtection = false;
+			}
+			// 3번 새로운 타일을 생성
+			newTileAdd();
+		}
+		if( e.getCode() == KeyCode.LEFT) {
+			for(int g = 0 ; g<board.length ; g++) {
+				// 1.  해당 방향으로 최대한 타일을 밀기
+				for(int i = 0 ; i<board.length ; i++) {
+					for(int k = 0 ; k<board.length-1 ; k++) {
+						if(board[k][i] == 0) {
+							board[k][i] = board[k+1][i];
+							board[k+1][i] = 0;
+						}
+					}
+				}
+			}
+			// 2. 해당 방향 부근에 인접한 숫자가 같은 숫자일경우 합친다.
+			for(int i = 0 ; i<board.length ; i++) {
+				for(int k = board.length-1 ; k>0 ; k--) {
+					if((board[k][i]==board[k-1][i])&&(!doubleSumProtection)&&(board[k][i]!=0)) {
+						board[k-1][i] *= 2;
+						board[k][i] = 0;
+						doubleSumProtection = true;
+					}
+				}
+				doubleSumProtection = false;
+			}
+			// 3번 새로운 타일을 생성
+			newTileAdd();
+		}
+		if( e.getCode() == KeyCode.RIGHT) {
+			for(int g = 0 ; g<board.length ; g++) {
+				// 1.  해당 방향으로 최대한 타일을 밀기
+				for(int i = 0 ; i<board.length ; i++) {
+					for(int k = board.length-1 ; k>0 ; k--) {
+						if(board[k][i] == 0) {
+							board[k][i] = board[k-1][i];
+							board[k-1][i] = 0;
+						}
+					}
+				}
+			}
+			// 2. 해당 방향 부근에 인접한 숫자가 같은 숫자일경우 합친다.
+			for(int i = 0 ; i<board.length ; i++) {
+				for(int k = 0 ; k<board.length-1 ; k++) {
+					if((board[k][i]==board[k+1][i])&&(!doubleSumProtection)&&(board[k][i]!=0)) {
+						board[k+1][i] *= 2;
+						board[k][i] = 0;
+						doubleSumProtection = true;
+					}
+				}
+				doubleSumProtection = false;
+			}
+			// 3번 새로운 타일을 생성
+			newTileAdd();
+		}
+	}
+
+	// 보드 상태 프린트(배열을 이용)
+	void boardPrint() {
+		for(int i = 0 ; i<4 ; i++) {
+			for(int k =0;k<4;k++) {
+				System.out.print(board[k][i]);
+			}
+			System.out.println("");
+		}
+		System.out.println("");
+	}
+
+	// 숫자에 따른 색 변경
+	void setColor(Label tile) {
+		if(tile.getText().equals("2"))				tile.setStyle("-fx-background-color:#D5CEC4; -fx-text-fill:#6D675D;-fx-alignment: center;-fx-font-size:30;"); //2
+		if(tile.getText().equals("4"))				tile.setStyle("-fx-background-color:#D2CAB5; -fx-text-fill:#6A645B;-fx-alignment: center;-fx-font-size:30;"); //4
+		if(tile.getText().equals("8"))				tile.setStyle("-fx-background-color:#D1A272; -fx-text-fill:#E4DFBB;-fx-alignment: center;-fx-font-size:30;"); //8
+		if(tile.getText().equals("16"))				tile.setStyle("-fx-background-color:#D08B5F; -fx-text-fill:#E1D3C5;-fx-alignment: center;-fx-font-size:30;"); //16
+		if(tile.getText().equals("32"))				tile.setStyle("-fx-background-color:#CF775A; -fx-text-fill:#E1D1C3;-fx-alignment: center;-fx-font-size:30;"); //32
+		if(tile.getText().equals("64"))				tile.setStyle("-fx-background-color:#CF5F40; -fx-text-fill:#E2D5C6;-fx-alignment: center;-fx-font-size:30;"); //64
+		if(tile.getText().equals("128"))			tile.setStyle("-fx-background-color:#D1BA6F; -fx-text-fill:#E4DEC3;-fx-alignment: center;-fx-font-size:30;"); //128
+		if(tile.getText().equals("256"))			tile.setStyle("-fx-background-color:#D4BD54; -fx-text-fill:#D8D6A9;-fx-alignment: center;-fx-font-size:30;"); //256
+		if(tile.getText().equals("512"))			tile.setStyle("-fx-background-color:#CCB664; -fx-text-fill:#E2D7BC;-fx-alignment: center;-fx-font-size:30;"); //512
+		if(tile.getText().equals("1024"))			tile.setStyle("-fx-background-color:#CEB349; -fx-text-fill:#E4E1BB;-fx-alignment: center;-fx-font-size:30;"); //1024
+		if(tile.getText().equals("2048"))			tile.setStyle("-fx-background-color:#D7C647; -fx-text-fill:#E2D8BA;-fx-alignment: center;-fx-font-size:30;"); //2048
+		if(Integer.parseInt(tile.getText())>=4096)	tile.setStyle("-fx-background-color:#37332E; -fx-text-fill:#C0BEBA;"); //4096
+	}
+
+	// 타일 추가하기
+	void newTileAdd() {
+		boardZeroCheck();
+		if(boardZeroCheckX.size()>0) {
+			randomCoord = (int)Math.floor(Math.random() * boardZeroCheckX.size());
+			randomNewTileText = (int)Math.floor(Math.random() * 100);
+			randomNewTileText = ( randomNewTileText < 70 ) ?2 : 4;
+			stackTile.push(new Label(String.valueOf(randomNewTileText)));
+			temp = (Label) stackTile.peek();
+			setColor(temp);
+			temp.setPrefSize(89, 89);
+			temp.setAlignment(Pos.CENTER);
+			board2048.add(temp, boardZeroCheckX.get(randomCoord), boardZeroCheckY.get(randomCoord));
+			board[boardZeroCheckX.get(randomCoord)][boardZeroCheckY.get(randomCoord)] = randomNewTileText;
+			System.out.println(boardZeroCheckX.get(randomCoord) +","+boardZeroCheckY.get(randomCoord)+"에 "+randomNewTileText+"추가");
+			board2048.setGridLinesVisible(true);
+			boardPrint();
+		}
+	}
+
+	// 보드 초기화
+	void initBoard() {
+		while(!stackTile.isEmpty()) {
+			board2048.getChildren().remove(stackTile.pop());
+		}
+		initArrayBoard();
+		// 초기에는 타일 2개 생성
+		newTileAdd();
+		newTileAdd();
+	}
 
 	@FXML
 	void onClickMainButton(MouseEvent e) throws IOException {
 		Node node = (Node)(e.getTarget());
-        stage = (Stage)(node.getScene().getWindow());
-        Parent root = FXMLLoader.load(getClass().getResource("MainUI.fxml"));
-        Scene scene = new Scene(root);
-        stage.setTitle("미니게임");
-        stage.setScene(scene);
-        stage.show();
+		stage = (Stage)(node.getScene().getWindow());
+		Parent root = FXMLLoader.load(getClass().getResource("MainUI.fxml"));
+		Scene scene = new Scene(root);
+		stage.setTitle("미니게임");
+		stage.setScene(scene);
+		stage.show();
 	}
-	
 	public void initialize() {
 		initBoard();
 		board2048.setFocusTraversable(true);
-}
+	}
 }
--- src/application/WordChainGameComputerController.java
+++ src/application/WordChainGameComputerController.java
@@ -25,24 +25,57 @@ public class WordChainGameComputerController {
     private int score;
     private ArrayList<String> wordList = new ArrayList<String>();
     private char lastWord;
+    private char firstWord;
+    private char subLastWord;
+    private boolean phoneticRule = false;
+    private String msgSubLastWord;
     @FXML private TextField inputWord;
     @FXML private Label lbMessage;
     @FXML private Label lbPrevWord;
     @FXML private Label lbScore;
-
-
+    @FXML private Label lbComputerWord;
     private final int minLetterCnt = 2;
 
-    String computerWord(){
+
+    void runPhoneticRule(){
+        char afterLetter[] = {'나', '낙', '난', '날', '남', '납', '낭', '내', '냉', '엽',
+                '노', '녹', '논', '농', '뇌', '용', '누', '윤', '율', '융',
+                '늑', '늠', '능', '인', '임', '입', '냑', '략', '냥', '량',
+                '녀', '려', '녁', '력', '년', '련', '녈', '렬', '념', '렴',
+                '녕', '령', '녜', '례', '뇨', '료', '뉴', '류', '뉵', '륙',
+                '니', '리'};
+        char beforeLetter[] = {'라', '락', '란', '랄', '람', '랍', '랑', '래', '랭', '렵',
+                '로', '록', '론', '롱', '뢰', '룡', '루', '륜', '률', '륭',
+                '륵', '름', '릉', '린', '림', '립', '약', '약', '양', '양',
+                '여', '여', '역', '역', '연', '연', '열', '열', '염', '염',
+                '영', '영', '예', '예', '요', '요', '유', '유', '육', '육',
+                '이', '이'};
+
+        for(int i = 0, letterLen = afterLetter.length; i < letterLen; i++){
+            if(lastWord == afterLetter[i]){
+                phoneticRule = true;
+                subLastWord = beforeLetter[i];
+                return;
+            }
+            if(lastWord == beforeLetter[i]){
+                phoneticRule = true;
+                subLastWord = afterLetter[i];
+                return;
+            }
+        }
+        phoneticRule = false;
+    }
+
+    String computerWord(char word){
         try {
             String findWord;
             int randomNum;
-            URL url = new URL("https://stdict.korean.go.kr/common/autoComplete.json?searchKeyword=임");
+            URL url = new URL("https://stdict.korean.go.kr/common/autoComplete.json?searchKeyword=" + word);
             HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             findWord = (br.readLine());
-// 단어 전처리 과정
+            // 단어 전처리 과정
             findWord = findWord.substring(findWord.indexOf("(")+1, findWord.indexOf(")"));
             String[] wordBox = findWord.split(",");
             for(int i=1 ; i<wordBox.length ; i++) {
@@ -64,6 +97,7 @@ public class WordChainGameComputerController {
         }
         return null;    // 컴퓨터가 입력할 단어가 없어서 패배
     }
+
     boolean isWord(String word){
         try {
             URL url = new URL("https://stdict.korean.go.kr/common/autoComplete.json?searchKeyword=" + word);
@@ -80,43 +114,31 @@ public class WordChainGameComputerController {
         }
         return false;
     }
-//  boolean isWord(String word){
-//  try {
-//      //시간 남으면 key 따로 분리할 예정
-//      String key = "0255BF41748A78C0AAFCAC21A3D9BE1D";
-//      URL url = new URL("https://stdict.korean.go.kr/api/search.do?key=" + key + "&type_search=search&q=" + word);
-//      String line = "";
-//      HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
-//      connection.setRequestMethod("GET");
-//      BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//      while ((line = br.readLine()) != null) {
-//          if(line.contains("<word>")) return true;
-//      }
-//      br.close();
-//  } catch (Exception e) {
-//      e.printStackTrace();
-//  }
-//  return false;
-//}
+
+    void setFirstWord(String word){
+        firstWord = word.charAt(0);
+        System.out.println("firstWord >> " + firstWord);
+    }
+
     void setLastWord(String word){
         lastWord = word.charAt(word.length() - 1);
         System.out.println("lastWord = " + lastWord);
     }
-
+    void setComWord(String word){
+        lbComputerWord.setText(word);
+    }
     void setMessage(String message){
         lbMessage.setText(message);
     }
-
     void addScore(){
         lbScore.setText(Integer.toString(++score));
     }
 
+
     @FXML
     void onPressEnter(KeyEvent e) {
-        System.out.println("score = " + score);
         if( e.getCode() == KeyCode.ENTER ) {
             String word = inputWord.getText();
-            System.out.println("size >> " + word.length());
             if(word.length() < minLetterCnt) {
                 setMessage("단어는 최소 " + minLetterCnt + "글자 이상부터 입력할 수 있습니다.");
                 return;
@@ -125,17 +147,34 @@ public class WordChainGameComputerController {
                 setMessage("중복된 단어입니다.");
                 return;
             }
-            // 새로 입력한 단어가 전에 입력한 마지막 글자와 같은지 비교
-            if((score>=1)&&(lastWord!=word.charAt(0))) {
-           		setMessage("시작 글자는[ " + lastWord + " ]입니다.");
-           		return;
+            if(phoneticRule){
+                setFirstWord(word);
+                if(!(firstWord == lastWord ^ firstWord == subLastWord)) return;
+            } else {
+                // 새로 입력한 단어가 전에 입력한 마지막 글자와 같은지 비교
+                if((score>=1)&&(lastWord!=word.charAt(0))) {
+                    setMessage("시작 글자는[ " + lastWord + " ]입니다.");
+                    return;
+                }
             }
             setMessage("");
             if(isWord(word)) {
                 wordList.add(word);
                 setLastWord(word);
+                runPhoneticRule();
+                String comWord;
+                if(phoneticRule) comWord = computerWord(subLastWord);
+                else comWord = computerWord(lastWord);
+                if(comWord != null) setComWord(comWord);
+                else setComWord("단어가 떠오르지 않아요 😥");
+                wordList.add(comWord);
                 lbPrevWord.setText(word);
-                setMessage("[ " + lastWord + " ]로 시작하는 단어를 입력하세요.");
+                setLastWord(comWord);
+                runPhoneticRule();
+                if(!phoneticRule) subLastWord = Character.MIN_VALUE;
+                if(subLastWord != Character.MIN_VALUE) msgSubLastWord = ", " + subLastWord;
+                else msgSubLastWord = "";
+                setMessage("[ " + lastWord + msgSubLastWord + " ]로 시작하는 단어를 입력하세요.");
                 inputWord.setText("");
                 addScore();
             } else setMessage("등록되지 않은 단어입니다.");
